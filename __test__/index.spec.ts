4e99961374ef224712ca0fe5aaa6fe9a6ad8b010
skidy89 <tutorialesyg310@gmail.com>
2025-10-25 23:50:11 -0600

    add initial test files and configurations for language loading and processing

---
+++ __test__/index.spec.ts
@@ -0,0 +1,12 @@
+import test from 'ava'
+import { loadLangs, generateTypescriptDefs } from '../index'
+import { Langs } from './languages'
+test('load languages', (t) => {
+    generateTypescriptDefs("__test__/", "__test__/languages.d.ts")
+    const e = loadLangs("__test__/") as Langs
+    t.is(e.e.hello, 'hello world')
+})
+test("generate typescript definitions", (t) => {
+    generateTypescriptDefs("__test__/", "__test__/languages.d.ts")
+    t.pass()
+})
